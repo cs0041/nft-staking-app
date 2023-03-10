cd9fe24ffbe91b5434c01d12cba03931a3170021
cs0041 <pamzalove20@hotmail.com>
2023-03-10 19:43:14 +0700

    contract address

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
-  "0x86eAf8D635eb11D5CD801c41744F44e50d4fd8d2";
+  "0x8E6f7fAA56DCE26Dd6CaBC1140112fBB450c973E";
 export const tokenContractAddress =
-  "0x12ea05ba6E8405cBeceB5b51134A821e86858F7E";
+  "0x6f7112AEaB55cCC715887324E033d268A75b3A3f";
 export const stakingContractAddress =
-  "0x830Ace54DFbf3a011601bD0Bc1f9806d45D2d0E4";
+  "0xe9d685856e00492586ed63202bb6dfc88dfac39d";
